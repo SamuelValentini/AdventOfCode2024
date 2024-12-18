49d1be7bd67a8302b7e96b832f3a09e32c11e5d0
Samuel Valentini <samuel.valentini@stalicla.com>
2024-12-18 10:48:46 +0100

    Solution to first problem

---
+++ Day18/solution1.py
@@ -0,0 +1,53 @@
+import heapq
+
+MAXDIM = 71
+TODROP = 1024
+
+matrix = []
+visited = []
+
+
+def bfs(visited, x, y):
+    queue = [(0, x, y)]
+    heapq.heapify(queue)
+    while len(queue) > 0 and not ((x == MAXDIM - 1) and (y == MAXDIM - 1)):
+        d, x, y = heapq.heappop(queue)
+        if visited[y][x] == 0:
+            visited[y][x] = 1
+            if (y - 1 >= 0) and not visited[y - 1][x]:
+                heapq.heappush(queue, (d + 1, x, y - 1))
+            if (x - 1 >= 0) and not visited[y][x - 1]:
+                heapq.heappush(queue, (d + 1, x - 1, y))
+            if not (y + 1 >= MAXDIM) and (not visited[y + 1][x]):
+                heapq.heappush(queue, (d + 1, x, y + 1))
+            if not (x + 1 >= MAXDIM) and (not visited[y][x + 1]):
+                heapq.heappush(queue, (d + 1, x + 1, y))
+
+    return d
+
+
+for i in range(MAXDIM):
+    l = []
+    v = []
+    for j in range(MAXDIM):
+        l.append(".")
+        v.append(0)
+    matrix.append(l)
+    visited.append(v)
+
+corrupted = []
+with open("./input.txt") as f:
+    for l in f:
+        l = l.strip().split(",")
+        corrupted.append((int(l[0]), int(l[1])))
+
+for i in range(TODROP):
+    x, y = corrupted[i]
+    matrix[y][x] = "#"
+    visited[y][x] = 1
+
+for l in matrix:
+    print(l)
+
+minPath = bfs(visited, 0, 0)
+print(minPath)
